791c8ad9bf9877b2b589bf71d9c6c843a81b3f7d
yuki <yukihiguchi0826@gmail.com>
2024-09-14 14:36:50 -0700

    implement VenueList

--- src/components/VenueList.tsx
+++ src/components/VenueList.tsx
@@ -1,10 +1,10 @@
-import { Box, Container, Paper, Typography } from "@mui/material";
+import { Box, Chip, Container, Paper, Typography } from "@mui/material";
 import venues from "../pages/data/venues.json"
 
 export const VenueList = () => {
   return (
     <Box>
-      <Container maxWidth="md">
+      <Container maxWidth="sm">
         <div>
         {venues.map((venue)=>{
           return(
@@ -12,9 +12,32 @@ export const VenueList = () => {
               <Paper>
                 <Typography variant="h5">{venue.name}</Typography>
                 <div>
-
+                  <div className="flex">
+                    <Typography sx={{width:80}}>Capacity:</Typography>
+                    <Typography>{venue.capacity}</Typography>
+                  </div>
+                  <div className="flex">
+                    <Typography sx={{width:80}}>Contact:</Typography>
+                    <Typography>{venue.contact}</Typography>
+                  </div>
+                  
+                  <div className="flex">
+                    <Typography sx={{width:80}}>Place type:</Typography>
+                    {venue.placeType.map((type, index) => {
+                      return(<Chip key={index} label={type} />)
+                    })}
+                  </div>
+                  <div className="flex">
+                  <Typography sx={{width:80}}>Events type:</Typography>
+                    {venue.eventsType.map((type, index) => {
+                      return(<Chip key={index} label={type} />)
+                    })}
+                  </div>
+                  <div className="flex">
+                    <Typography sx={{width:80}}>Address:</Typography>
+                    <Typography>{venue.address}</Typography>
+                  </div>
                 </div>
-                {venue.address}
               </Paper>
             </div>
           )
