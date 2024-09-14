e5fb2a43a050ec164b1091092a869adaebd3f8ec
yuki <yukihiguchi0826@gmail.com>
2024-09-14 14:38:44 -0700

    add Card

--- src/components/VenueList.tsx
+++ src/components/VenueList.tsx
@@ -1,4 +1,4 @@
-import { Box, Chip, Container, Paper, Typography } from "@mui/material";
+import { Box, Card, CardContent, Chip, Container, Paper, Typography } from "@mui/material";
 import venues from "../pages/data/venues.json"
 
 export const VenueList = () => {
@@ -8,37 +8,40 @@ export const VenueList = () => {
         <div>
         {venues.map((venue)=>{
           return(
-            <div key={venue.id}>
-              <Paper>
-                <Typography variant="h5">{venue.name}</Typography>
-                <div>
-                  <div className="flex">
-                    <Typography sx={{width:80}}>Capacity:</Typography>
-                    <Typography>{venue.capacity}</Typography>
+            <div key={venue.id} className="my-3">
+              <Card>
+                <CardContent>
+
+                  <Typography variant="h5">{venue.name}</Typography>
+                  <div>
+                    <div className="flex">
+                      <Typography sx={{width:80}}>Capacity:</Typography>
+                      <Typography>{venue.capacity}</Typography>
+                    </div>
+                    <div className="flex">
+                      <Typography sx={{width:80}}>Contact:</Typography>
+                      <Typography>{venue.contact}</Typography>
+                    </div>
+                    
+                    <div className="flex">
+                      <Typography sx={{width:80}}>Place type:</Typography>
+                      {venue.placeType.map((type, index) => {
+                        return(<Chip key={index} label={type} />)
+                      })}
+                    </div>
+                    <div className="flex">
+                    <Typography sx={{width:80}}>Events type:</Typography>
+                      {venue.eventsType.map((type, index) => {
+                        return(<Chip key={index} label={type} />)
+                      })}
+                    </div>
+                    <div className="flex">
+                      <Typography sx={{width:80}}>Address:</Typography>
+                      <Typography>{venue.address}</Typography>
+                    </div>
                   </div>
-                  <div className="flex">
-                    <Typography sx={{width:80}}>Contact:</Typography>
-                    <Typography>{venue.contact}</Typography>
-                  </div>
-                  
-                  <div className="flex">
-                    <Typography sx={{width:80}}>Place type:</Typography>
-                    {venue.placeType.map((type, index) => {
-                      return(<Chip key={index} label={type} />)
-                    })}
-                  </div>
-                  <div className="flex">
-                  <Typography sx={{width:80}}>Events type:</Typography>
-                    {venue.eventsType.map((type, index) => {
-                      return(<Chip key={index} label={type} />)
-                    })}
-                  </div>
-                  <div className="flex">
-                    <Typography sx={{width:80}}>Address:</Typography>
-                    <Typography>{venue.address}</Typography>
-                  </div>
-                </div>
-              </Paper>
+                </CardContent>
+              </Card>
             </div>
           )
         })}
